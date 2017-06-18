d40ca5baeef38536991c0d43593e6adb6336fca1
Alko89 <alefer89@gmail.com>
2017-06-18 15:01:45 +0200

    pagination

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,32 @@
 jQuery(function($){
+    var page = 0;
+    var pages;
+    var items_per_page = 10;
+
+    var txt;
+
     magnetSearch();
 
     $('#search').keyup(function(){
         magnetSearch();
     });
+    $("#nav select").on("change", function() {
+        page = this.value;
+        magnetSearch();
+    })
 
     function magnetSearch(){
-        var txt = $('#search').val();
+        txt = $('#search').val();
         if (txt == "")
             txt = " ";
 
         $.ajax({
             type: "GET",
-            url: "/magnet/search/" + txt + "/0/100"
+            url: "/magnet/search/" + txt + "/" + page + "/" + items_per_page
         }).done(function(result) {
             $("#table tbody").empty();
+
+            pagination(result.magnets_filtered);
             
             $.each(result.magnets, function(i, item) {
                 var $tr = $('<tr>').append(
@@ -28,5 +40,20 @@ jQuery(function($){
                 ).appendTo("#table tbody");
             });
         });
+    }
+    
+    function pagination(filtered){
+            pages = filtered / items_per_page;
+            $("#nav select").empty();
+            
+//             if (pages < 10) {
+                for (var i = 0; i < pages; i++){
+                    if (i == page)
+                        var option = $('<option>').attr("value", i).attr("selected", "true").text(i + 1);
+                    else
+                        var option = $('<option>').attr("value", i).text(i + 1);
+                    $("#nav select").append(option);
+                }
+//             }
     }
 });
